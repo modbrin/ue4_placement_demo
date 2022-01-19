3445d50b2250764f50be92c50001641fa50c868d
Maksim Surkov <modbrin@gmail.com>
2022-01-19 14:44:25 +0500

    Project structure refactor

---
+++ Source/PlacementDemo/Public/PaletteCategory.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "PaletteCategory.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLACEMENTDEMO_API UPaletteCategory : public UObject
+{
+	GENERATED_BODY()
+	
+};
